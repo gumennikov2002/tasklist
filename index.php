54205754566df3225e364299084f31da2ed0d5d3
gumennikov2002 <gumennikov24@yandex.ru>
2021-06-04 15:27:13 +0400

    Добавил файлы входа/регистрации

--- index.php
+++ index.php
@@ -6,4 +6,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <title>TaskList</title>
     </head>
+    <body>
+        <div class="container">
+            
+        </div>
+    </body>
 </html>

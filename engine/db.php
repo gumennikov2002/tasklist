9e0c211690cbe66553277ae3a08607c04c0a6db1
gumennikov2002 <gumennikov24@yandex.ru>
2021-06-07 10:26:08 +0400

    Настроил подключение к БД

---
+++ engine/db.php
@@ -0,0 +1,8 @@
+<?php
+    $db_host = "localhost";
+    $db_name = "tasklist";
+    $db_login = "root";
+    $db_pass = "root";
+
+    $db = mysqli_connect($host, $db_login, $db_pass, $db_name);
+?>
